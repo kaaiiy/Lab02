fd7b0e0a500c111dffb3f2f73e7d840037bacebb
bsha01 <90475686+bsha01@users.noreply.github.com>
2021-09-10 18:39:19 -0700

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301338232 - This line was added by Kaitlyn Yu.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301253251 - This line was added by Brian Shao.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
