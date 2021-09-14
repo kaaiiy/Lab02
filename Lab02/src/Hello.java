85fb9e50f5e58eb59be986ecc2009d9a5dbbe683
Rachel Mao <rachelrmao99@hotmail.com>
2021-09-13 17:25:23 -0700

    Update SacStateID in Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("301338232 - This line was added by Kaitlyn Yu.");
 		System.out.println("301253251 - This line was added by Brian Shao.");
-		System.out.println("SacStateID - This line was added by Rachel Mao.");
+		System.out.println("218715394 - This line was added by Rachel Mao.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
